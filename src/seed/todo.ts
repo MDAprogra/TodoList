606733f28b8741f96f5dd165d4f7890acc6c581b
matthiasd <matth4627@gmail.com>
2025-12-10 11:20:53 +0100

    Seeder

---
+++ src/seed/todo.ts
@@ -0,0 +1,24 @@
+import 'dotenv/config'
+import { prisma } from "@/lib/prisma";
+import { error } from "console";
+import { faker } from '@faker-js/faker';
+
+const todosToCreate = Array.from(Array(10)).map(() => ({
+    label : faker.lorem.sentence(),
+    priority : faker.helpers.arrayElement(['HIGH','MEDIUM','LOW']),
+    status : faker.helpers.arrayElement(['CHECKED','NOT_CHECKED'])
+}))
+
+try{
+const createdTodo = await prisma.todo.createMany(
+    {
+        data : todosToCreate
+    }   
+)
+console.log('✅ Todos created ', createdTodo);
+}
+catch{
+    console.log('❌ Error while creating todo', error);
+}
+
+
