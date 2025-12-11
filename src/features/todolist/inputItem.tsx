b118aeef6e11f51013e676f4d084353b68171ae2
matthiasd <matth4627@gmail.com>
2025-12-11 12:12:12 +0100

    Create mutation to insert task to todo

--- src/features/todolist/inputItem.tsx
+++ src/features/todolist/inputItem.tsx
@@ -1,5 +1,12 @@
-import { Field, FieldDescription, FieldError, FieldLabel } from '@/components/ui/field';
+import {
+  Field,
+  FieldDescription,
+  FieldError,
+  FieldLabel,
+} from '@/components/ui/field';
 import { Input } from '@/components/ui/input';
+import { Todo } from '@/generated/prisma/client';
+import { useMutation } from '@tanstack/react-query';
 import { SubmitHandler, useForm } from 'react-hook-form';
 
 type CreateTodoFormValues = {
@@ -7,28 +14,52 @@ type CreateTodoFormValues = {
 };
 
 export const InputItem = () => {
+    const { mutate } = useMutation({
+    mutationFn: async (todo: Todo) => {
+      const result = await fetch('/api/todo', {
+        method: 'POST',
+        body: JSON.stringify(todo),
+      });
+
+      return (await result.json()) as { data: Todo };
+    },
+    onSuccess: (_data, _vars, _onMutate, ctx) => {
+      ctx.client.invalidateQueries({ queryKey: ['todos'] });
+      reset();
+    },
+  });
+
   const {
     register,
     handleSubmit,
     formState: { errors },
+    reset,  
   } = useForm<CreateTodoFormValues>();
-  const onSubmit: SubmitHandler<CreateTodoFormValues> = (data) =>
-    console.log(data);
+const onSubmit: SubmitHandler<CreateTodoFormValues> = (data) => {
+    mutate({
+        id: '',
+        label: data.label,
+        status: 'NOT_CHECKED',
+        priority: 'MEDIUM',
+        deadline: null,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+    });
+    
+};
 
   return (
-    <form 
-      onSubmit={handleSubmit(onSubmit)}
-      className="flex gap-3 w-full p-3">
-        <Field>
-            <FieldLabel htmlFor="input-id">Label</FieldLabel>
-            <Input
-            {...register('label', {
-                required: "Veuillez completer le champ 'LABEL' !",
-            })}
-            />
-            <Input type="submit"/>
-            {errors.label &&(<FieldError>{errors.label.message}</FieldError>)}
-        </Field>
+    <form onSubmit={handleSubmit(onSubmit)} className="flex gap-3 w-full p-3">
+      <Field>
+        <FieldLabel htmlFor="input-id">Label</FieldLabel>
+        <Input
+          {...register('label', {
+            required: "Veuillez completer le champ 'LABEL' !",
+          })}
+        />
+        <Input type="submit" />
+        {errors.label && <FieldError>{errors.label.message}</FieldError>}
+      </Field>
     </form>
   );
 };
